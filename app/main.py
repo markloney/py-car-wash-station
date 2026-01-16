3fa14bb8ee0aa275a50fee0281a09d84b89a2dea
Mark Loney <markloney@loney.me>
2026-01-16 16:47:51 +0000

    Solution v4

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ class CarWashStation:
             if car.clean_mark < self.clean_power:
                 income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
-        return income
+        return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
         if self.clean_power > car.clean_mark:
